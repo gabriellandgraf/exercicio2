5f50534a27d2a86dc5bdd22b9ec615b20d5de78d
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-07-23 17:20:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+
+    <?php
+        $nome="Gabriel";
+        $cidade="Cornélio Procópio";
+        $numeroCasa=57;
+        $numeroChamada=7;
+
+        /*se (isso acontecer) {
+            faça isso
+        }*/
+
+        if ($numeroCasa>$numeroChamada) {
+            echo ' verdadeiro';
+        } else {
+            echo 'falso';
+        }
+    ?>
+
+</body>
+
+</html>
